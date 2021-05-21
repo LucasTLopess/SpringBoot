0999f68cf25ddd701da13d38a513ae2e0554502d
Lucas Troleiz Lopes <troleizlucas@gmail.com>
2021-05-21 20:44:16 -0300

    Update TwilioConfig.java

--- notification/src/main/java/br/com/compasso/notificacao/notificationApi/service/TwilioConfig.java
+++ notification/src/main/java/br/com/compasso/notificacao/notificationApi/service/TwilioConfig.java
@@ -3,8 +3,8 @@ package br.com.compasso.notificacao.notificationApi.service;
 public class TwilioConfig {
 
 
-    final String ACCOUNT_SID ="ACfea9cd549bb7be7e7f9ae4b84096284f";
-    final String AUTH_TOKEN ="b25cce86aa3f13a99e22f84a65a54167";
+    final String ACCOUNT_SID ="YOUR ACCOUNT SID";
+    final String AUTH_TOKEN ="YOUR AUTH TOKEN";
     final String phoneNumber = "+5162199912";
 
     public String getPhoneNumber() {
