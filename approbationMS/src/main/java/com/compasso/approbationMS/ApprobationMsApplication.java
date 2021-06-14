4e9e653ae11804d6fc48d497a3f640b12c16d576
Lucas T Lopes <troleizlucas@gmail.com>
2021-06-14 09:49:29 -0300

    criando esqueleto das API'S

---
+++ approbationMS/src/main/java/com/compasso/approbationMS/ApprobationMsApplication.java
@@ -0,0 +1,13 @@
+package com.compasso.approbationMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprobationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprobationMsApplication.class, args);
+	}
+
+}
